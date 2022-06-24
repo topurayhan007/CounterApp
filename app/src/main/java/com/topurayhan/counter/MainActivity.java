9cc00c7226ab999ece4aa4481f755f272b712166
topurayhan007 <topurayhan007@gmail.com>
2022-06-25 01:49:01 +0600

    DONE

---
+++ app/src/main/java/com/topurayhan/counter/MainActivity.java
@@ -0,0 +1,43 @@
+package com.topurayhan.counter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+    int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void resetAndToast(View view) {
+        Toast.makeText(getApplicationContext(), "Counter has been reset!", Toast.LENGTH_SHORT).show();
+        TextView text = findViewById(R.id.counterText);
+        counter = 0;
+        text.setText(Integer.toString(counter));
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void incrementCounter(View view) {
+        TextView text = findViewById(R.id.counterText);
+        counter++;
+        if (counter < 1000) {
+            text.setText(Integer.toString(counter));
+        } else {
+            Toast.makeText(getApplicationContext(), "Maximum reached! Reset to start again.", Toast.LENGTH_SHORT).show();
+            Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            vibrator.vibrate(100);
+        }
+    }
+}
